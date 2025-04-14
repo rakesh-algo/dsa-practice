3222c5b7555cd72ba6597864e00d030d24887c18
Rakesh <rakeshk1195@gmail.com>
2025-04-14 15:32:43 +0530

    feat: implement function to print enter-aligned pyramid pattern of stars

---
+++ patterns/pattern4.js
@@ -0,0 +1,10 @@
+// Function to print enter-aligned pyramid pattern of stars
+function print(n) {
+  for (let row = 1; row <= n; ++row) {
+    const spaces = ' '.repeat(n - row);
+    const stars = '*'.repeat(2 * row - 1);
+    console.log(spaces + stars);
+  }
+}
+
+print(5);
