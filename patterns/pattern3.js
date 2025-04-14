f338d26a93718798c745da88f3000d596c75ae6b
Rakesh <rakeshk1195@gmail.com>
2025-04-14 15:06:46 +0530

    feat: implement function to print right aligned triangle pattern of stars

---
+++ patterns/pattern3.js
@@ -0,0 +1,10 @@
+// Function to print right aligned triangle pattern of stars
+function print(n) {
+  for (let row = 1; row <= n; ++row) {
+    const spaces = ' '.repeat(n - row);
+    const stars = '*'.repeat(row);
+    console.log(spaces + stars);
+  }
+}
+
+print(1);
