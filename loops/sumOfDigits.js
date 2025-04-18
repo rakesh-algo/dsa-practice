58805a5af54a1f3de4b49dfea7883f29dbe9ae93
Rakesh <rakeshk1195@gmail.com>
2025-04-18 21:22:38 +0530

    feat: implement function to calcuate sum of digits of a number

---
+++ loops/sumOfDigits.js
@@ -0,0 +1,13 @@
+function sumOfDigits(num) {
+  let sum = 0;
+  let remainder;
+
+  while (num !== 0) {
+    remainder = num % 10;
+    sum = sum + remainder;
+    num = Math.trunc(num / 10);
+  }
+  return sum;
+}
+
+console.log(sumOfDigits(12345)); // 15
